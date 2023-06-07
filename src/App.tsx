34ae745b0e6a55ce5bd42aa5380c8642b49e80f9
Stenstromen <kontakt@filipstenstrom.se>
2023-06-07 17:04:29 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+import Collapse from "react-bootstrap/Collapse";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "./App.css";
 import { IPredictions, TSorted } from "./Types";
-import { submitFile } from "./Api";
 import File from "./Components/File";
 import Camera from "./Components/Camera";
 
 function App() {
-  const [file, setFile] = useState<File | null>(null);
   const [predictions, setPredictions] = useState<IPredictions>({});
   const [sortedPredictions, setSortedPredictions] = useState<TSorted>([]);
   const [showFile, setShowFile] = useState<boolean>(false);
   const [showCamera, setShowCamera] = useState<boolean>(false);
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) return setFile(event.target.files[0]);
-  };
-
-  const handleFileSubmit = async () => {
-    if (file) return setPredictions(await submitFile(file));
-  };
 
   useEffect(() => {
     setSortedPredictions(
@@ -32,45 +30,85 @@ function App() {
   }, [sortedPredictions]);
 
   return (
-    <>
-      {showFile ? (
-        <>
-          <File setPredictions={setPredictions} />
-          <button onClick={() => setShowFile(false)}>Hide File</button>
-        </>
-      ) : (
-        <button onClick={() => setShowFile(true)}>Show File</button>
-      )}
-      {showCamera ? (
-        <>
-          <Camera setPredictions={setPredictions} />
-          <button onClick={() => setShowCamera(false)}>Hide Camera</button>
-        </>
-      ) : (
-        <button onClick={() => setShowCamera(true)}>Show Camera</button>
-      )}
-      <div>
-        {sortedPredictions.map((prediction, index) => (
-          <div key={prediction[0]}>
-            {index == 0 && (
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+      minBreakpoint="xxs"
+    >
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs={6} md={4}>
+            {showFile ? (
               <>
-                <strong>High Confidence</strong>
-                <br />
+                <File setPredictions={setPredictions} />
+                <button onClick={() => setShowFile(false)}>Hide File</button>
               </>
+            ) : (
+              !showCamera && (
+                <Button
+                  variant="outline-primary"
+                  className="p-5"
+                  onClick={() => setShowFile(true)}
+                >
+                  Show File
+                </Button>
+              )
             )}
-            {index == 0 && prediction[0].split(",")[0]}
-            {index == 1 && (
+          </Col>
+          <Col xs={6} md={4}>
+            {showCamera ? (
               <>
-                <br />
-                <strong>Low Confidence</strong>
-                <br />
+                <Camera setPredictions={setPredictions} />
+                <button onClick={() => setShowCamera(false)}>
+                  Hide Camera
+                </button>
               </>
+            ) : (
+              !showFile && (
+                <Button variant="outline-primary" className="p-5" onClick={() => setShowCamera(true)}>Show Camera</Button>
+              )
             )}
-            {index >= 1 && index <= 4 && prediction[0].split(",")[0]}
-          </div>
-        ))}
-      </div>
-    </>
+          </Col>
+        </Row>
+      </Container>
+      <Collapse in={sortedPredictions.length > 0 ? true : false}>
+        <div>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Confidence</Card.Title>
+              <Card.Subtitle className="mb-1 text-success">High</Card.Subtitle>
+              {sortedPredictions.map((prediction, index) => (
+                <>
+                  {index == 0 && (
+                    <Card.Text>
+                      <strong>
+                        {prediction[0].split(",")[0].charAt(0).toUpperCase() +
+                          prediction[0].split(",")[0].slice(1)}
+                      </strong>
+                    </Card.Text>
+                  )}
+                </>
+              ))}
+              <Card.Subtitle className="mb-1 text-warning">Low</Card.Subtitle>
+              <Card.Text>
+                {sortedPredictions.map((prediction, index) => (
+                  <>
+                    {index >= 1 && index <= 4 && (
+                      <>
+                        <strong>
+                          {prediction[0].split(",")[0].charAt(0).toUpperCase() +
+                            prediction[0].split(",")[0].slice(1)}
+                        </strong>
+                        <br />
+                      </>
+                    )}
+                  </>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Collapse>
+    </ThemeProvider>
   );
 }
 
