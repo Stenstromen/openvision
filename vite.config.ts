265ae460db694d6f855c725160deb882b4e3cf14
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-06-21 20:49:31 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 export default defineConfig({
   server: { https: true },
-  plugins: [ mkcert() ]
-})
+  plugins: [mkcert()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+});
